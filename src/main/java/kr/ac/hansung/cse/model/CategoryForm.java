363057bdcdfefe0127aed07c4b76648402575484
범준 <beomjun@beomjun-ui-MacBookAir.local>
2026-04-14 16:09:40 +0900

    feat: CategoryForm DTO 추가

---
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -0,0 +1,16 @@
+package kr.ac.hansung.cse.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryForm {
+    @NotBlank (message = "카테고리 이름을 입력하세요")
+    @Size(max = 50, message = "50자 이내로 입력하세요")
+    private String name;
+}
